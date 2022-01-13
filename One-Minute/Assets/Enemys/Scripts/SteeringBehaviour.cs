bbc1dd34b19b8d3b36e39e16a0f865aed4024b65
Vakoum <filipkuzmicz@yahoo.com>
2022-01-13 17:00:31 +0100

    pull

---
+++ One-Minute/Assets/Enemys/Scripts/SteeringBehaviour.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ContextSteeringBehaviour", menuName = "Steering Behaviour", order = 0)]
+public class ContextSteeringBehaviourSetup : ScriptableObject
+{
+    [Header("Context Variables")]
+    [Range(1, 50)]
+    public float circleRadius;
+    [Range(1, 50)]
+    public int resolution;
+    [Range(0.0f, 1f)]
+    public float steeringSmoothingFactor;
+    [Range(-1.0f, 0f)]
+    public float avoidAvoidanceWeight;
+    [Range(-1.0f, 0f)]
+    public float danagerAvoidanceWeight;
+
+    [Header("Mask Filters")]
+    public LayerMask interestLayermask;
+    public LayerMask avoidLayerMask;
+    public LayerMask dangerLayermask;
+
+    [Header("Debug")]
+    public bool showDebugLines;
+}
