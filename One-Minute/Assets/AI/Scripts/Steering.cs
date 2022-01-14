f2266b00f843c85911940869f5e88ce9a36b0a62
Vakoum <filipkuzmicz@yahoo.com>
2022-01-14 22:03:33 +0100

    sheesh

--- One-Minute/Assets/AI/Scripts/Steering.cs
+++ One-Minute/Assets/AI/Scripts/Steering.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Steering : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(Input.mousePosition);
     }
 }
